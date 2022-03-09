94704d6d5aa440a5e4453a1286e8d9d26f4acd52
Dylan Soll <22dylansoll@gmail.com>
2022-03-09 23:41:05 +1000

    Open CV - H cascade

--- opencv/create_image_selection.py
+++ opencv/create_image_selection.py
@@ -1,11 +1,30 @@
 import cv2
-cap = cv2.VideoCapture(0)
-for i in range(100):
-    run = True
+import time
+def create_im_from_cam(iterations):
+    cap = cv2.VideoCapture(0)
+    for i in range(iterations):
+        run = True
+        while run:
+            ret, image = cap.read()
+            cv2.imshow('Test', image)
+            if cv2.waitKey(1) == 32:
+                run = False
+        cv2.imwrite("images/-frame%d.jpg" % i, image) # save frame as JPEG file.
+    cap.release()
+    return
+
+def create_video():
+    run= True
+    cap = cv2.VideoCapture(0)
+    frame_size = (int(cap.get(3)), int(cap.get(4)))
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter(f"{time.time()}.mp4", fourcc, 20, frame_size)
     while run:
         ret, image = cap.read()
         cv2.imshow('Test', image)
         if cv2.waitKey(1) == 32:
             run = False
-    cv2.imwrite("images/p-frame%d.jpg" % i, image) # save frame as JPEG file.
-    
+            out.release()
+    cap.release()
+
+create_im_from_cam(20)
--- opencv/cvinterface.py
+++ opencv/cvinterface.py
@@ -1,23 +1,24 @@
 import numpy as np
 import cv2
+from PIL import ImageGrab
 cap = cv2.VideoCapture(0)
+def find_h(frame):
+    h_cascade = cv2.CascadeClassifier("C:/Users/22dyl\OneDrive\Desktop/h_training_data_cropped\classifier\cascade.xml")
+    h = h_cascade.detectMultiScale(frame, 1.3, 5)
+    return h
+
+
 while True:
     var, frame = cap.read()
-    f_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    e_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = f_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = e_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    #frame = cv2.Canny(frame, 100, 50)
+    h = find_h(frame)
+    print(h)
+    for (x, y, width, height) in h:
+        #temp_img = np.array(ImageGrab.grab(bbox=(x,y,x+width, y+height)))
+        #cv2.imshow('Frame', temp_img)
+        #cv2.waitKey(0)
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
     cv2.imshow('img', frame)
     
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        cv2.destroyAllWindows()
-        break
+    if cv2.waitKey(1) == ord('q'):
+        break 
+
